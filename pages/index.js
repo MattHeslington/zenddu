128d604160771a992eae928073a3638071a02624
Matt Heslington <matt.heslington@icloud.com>
2021-01-19 11:46:14 +0800

    🎉 First commit. Tailwind added and working

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div className="bg-green-400">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <h2 className="font-bold">test</h2>
+        </div>
+    )
+}
